9ba034c321577363264f1bd973599ba7466d9a5e
mericadiguzel <meric.adiguzel@gmail.com>
2023-01-16 00:47:45 +0300

    first commit

---
+++ Constants.playground/Contents.swift
@@ -0,0 +1,16 @@
+
+    
+    
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    
+    //The number of letters in alphabet equals 26
+    
+    
+let a = alphabet[Int.random(in: 12 ..< 13)]
+let b = alphabet[Int.random(in: 4 ..< 5)]
+let c = alphabet[Int.random(in: 17 ..< 18)]
+let d = alphabet[Int.random(in: 8 ..< 9)]
+let e = alphabet[Int.random(in: 2 ..< 3)]
+
+var password = [a + b + c + d + e]
+print(password)
